5e0186baa3579ae56e7de4246ba4ac8407061bcb
kubyser <kubiks@gmail.com>
2025-12-05 23:15:26 +0000

    Day5 update

--- Day5.py
+++ Day5.py
@@ -3,48 +3,34 @@ def within(a, r):
 
 data = [s.removesuffix("\n") for s in open("resources/day5_input.txt", "r")]
 freshRanges = []
-available = []
 for s in data:
     if s == "":
         break
-    ss = [int(x) for x in s.split("-")]
-    freshRanges.append(ss)
-for s in data[len(freshRanges)+1:]:
-    available.append(int(s))
-numFresh = 0
-for x in available:
-    fresh = False
-    for r in freshRanges:
-        if x >= r[0] and x <= r[1]:
-            fresh = True
-            break
-    if fresh:
-        numFresh += 1
+    freshRanges.append([int(x) for x in s.split("-")])
+available= [int(s) for s in data[len(freshRanges)+1:]]
+numFresh = sum([1 if any(within(x, r) for r in freshRanges) else 0 for x in available])
 print(numFresh)
 
 newRanges = [(freshRanges[0], True)]
 for fr in freshRanges[1:]:
     nr = [fr[0], fr[1]]
-    for i,rRec  in enumerate(newRanges):
-        r = rRec[0]
-        isValid = rRec[1]
+    for i,(r, isValid) in enumerate(newRanges):
         if not isValid:
             continue
         if within(r[0], nr) and within(r[1], nr):
-            rRec = (r, False)
-            newRanges[i] = rRec
+            newRanges[i] = (r, False)
             continue
         if within(nr[0], r):
             if within(nr[1], r):
-                nr = []
+                nr = None
             else:
                 nr[0] = r[1]+1
         elif within(nr[1], r):
             nr[1] = r[0]-1
-        if len(nr) == 0 or nr[0] > nr[1]:
+        if nr is None or nr[0] > nr[1]:
             break
-    if len(nr) > 0 and nr[0] <= nr[1]:
+    if nr is not None and nr[0] <= nr[1]:
         newRanges.append((nr, True))
 sumRanges = sum(r[0][1]-r[0][0]+1 for r in newRanges if r[1])
-print(newRanges)
+#print(newRanges)
 print(sumRanges)
