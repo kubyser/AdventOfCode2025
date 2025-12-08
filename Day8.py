b8f786cc727c49de8335067653477f8bda6a5d72
kubyser <kubiks@gmail.com>
2025-12-08 22:46:04 +0000

    Day 8 update

--- Day8.py
+++ Day8.py
@@ -1,30 +1,22 @@
 import math
+from time import time
 
 data = [s.removesuffix("\n") for s in open("resources/day8_input.txt", "r")]
 coords = [(int(s.split(',')[0]), int(s.split(',')[1]), int(s.split(',')[2])) for s in data]
-connections = {}
-circuits = {}
-circMap = {}
-for i,c in enumerate(coords):
-    connections[c] = set()
-    circuits[i] = {c}
-    circMap[c] = i
+connections = {c: set() for c in coords}
+circuits = {i:{c} for i,c in enumerate(coords)}
+circMap = {c:i for i,c in enumerate(coords)}
 numIterations = 1000
-distances = []
-for a,first in enumerate(coords[:-1]):
-    for b,second in enumerate(coords[a+1:]):
-        dist = math.sqrt(math.pow(first[0]-second[0], 2) +
-                         math.pow(first[1]-second[1], 2) +
-                         math.pow(first[2]-second[2], 2))
-        distances.append((dist, first, second))
+startTime = time()
+distances = [((math.pow(first[0]-second[0], 2) +
+               math.pow(first[1]-second[1], 2) +
+               math.pow(first[2]-second[2], 2)), first, second) for a,first in enumerate(coords[:-1]) for second in coords[a+1:]]
 distances.sort(key = lambda x: x[0], reverse=True)
 i = 0
 #for i in range(numIterations):
 while True:
 #    print(f"iteration {i}")
-    minD = distances.pop()
-    minPair = (minD[1], minD[2])
-    first, second = minPair
+    minD, first, second = distances.pop()
     connections[first].add(second)
     if (i := i+1) == numIterations:
         sizes = [len(circuits[c]) for c in circuits]
@@ -42,4 +34,6 @@ while True:
     if len(circuits) == 1:
         res = first[0] * second[0]
         print(f"Part 2 res: {res}")
-        exit(1)
+        break
+endTime = time()
+print(f"Elapsed time: {endTime-startTime}")
