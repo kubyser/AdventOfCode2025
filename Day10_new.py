9a26a0a20136845c205beb70bb633320c3fb844b
kubyser <kubiks@gmail.com>
2025-12-12 02:32:04 +0000

    Day 10 equations

---
+++ Day10_new.py
@@ -0,0 +1,81 @@
+from copy import deepcopy
+
+def reduce(equations):
+    while True:
+        reduced = False
+        restart = False
+        for i, (elements, val) in enumerate(equations):
+            for j, (testElements, testVal) in enumerate(equations):
+                if j == i:
+                    continue
+                if any(x not in testElements for x in elements):
+                    continue
+                if val > testVal:
+                    return None
+                reduced = True
+                testElements -= elements
+                if len(testElements) == 0:
+                    equations.pop(j)
+                    restart = True
+                    break
+                else:
+                    equations[j] = (testElements, testVal - val)
+            if restart:
+                break
+        if not reduced:
+            break
+    return equations
+
+def solve(equations):
+    global minValue
+    equations = deepcopy(equations)
+    equations = reduce(equations)
+    if equations is None:
+        return None
+    if all(len(x[0]) == 1 for x in equations):
+        summ = sum([x[1] for x in equations])
+        if minValue is None or minValue > summ:
+            minValue = summ
+        #print(f"Solved! {equations} Sum={summ} minValue={minValue}")
+        return equations
+    minEq = min([x for x in equations if len(x[0]) > 1], key = lambda x: x[1])
+    for firstVar in minEq[0]:
+        break
+    #print(minEq, firstVar)
+    maxValue = min([x[1] for x in equations if firstVar in x[0]])
+    equations.append(({firstVar}, None))
+    for newValue in range(maxValue+1):
+        equations[-1] = ({firstVar}, newValue)
+        solve(equations)
+    #print("Tried all")
+    return equations
+
+data = [s.removesuffix("\n") for s in open("resources/day10_input.txt", "r")]
+tasks = []
+for s in data:
+    s = s.split()
+    bs = s[1:-1]
+    ts = s[-1][1:-1]
+    target = [int(x) for x in ts.split(",")]
+    buttons = []
+    for b in bs:
+        button = set()
+        for x in b[1:-1].split(","):
+            button.add(int(x))
+        buttons.append(button)
+    tasks.append((target, buttons))
+totalSum = 0
+for nt, (target, buttons) in enumerate(tasks):
+    print(f"========= Task {nt} ============")
+    minValue = None
+    equations = []
+    for pos,x in enumerate(target):
+        elements = {i for i, b in enumerate(buttons) if pos in b}
+        equations.append((elements, x))
+    equations = solve(equations)
+    print(f"task {nt} completed, minValue={minValue}")
+    totalSum += minValue
+print(totalSum)
+
+
+
